1b082684e91844452ddff6eeeb91702614be35c2
Morten Hustveit <morten@rashbox.org>
2009-11-15 23:09:41 +0100

    Compose code.

--- vinkweb/compose.php
+++ vinkweb/compose.php
@@ -2,15 +2,32 @@
 require_once('config.inc.php');
 require_once('session.inc.php');
 
-if(array_key_exists($_POST['vink-message']))
+if(array_key_exists('vink-message', $_POST))
 {
+  @pg_query($vink, 'BEGIN');
   @pg_query_params($vink, 'INSERT INTO vink_blips (owner, body) VALUES ($1, $2)',
                    array($vink_userid, $_POST['vink-message']));
 
-  /*
+  $blip_id_res
+    = pg_query_params($vink, 'SELECT CURRVAL($1)', array('vink_blips_id_seq'));
+  $blip_id_row = pg_fetch_row($blip_id_res);
+  $blip_id = $blip_id_row[0];
+
+  $participants = $_POST['vink-participants'];
+
+  foreach($participants as $participant)
+  {
+    /* XXX: Verify that participant is in user's address list */
+
+    pg_query_params($vink, 'INSERT INTO vink_participants (blip_id, contact) VALUES ($1, $2)',
+                    array($blip_id, $participant));
+  }
+
+ /*
    "SELECT CURRVAL(
      pg_get_serial_sequence('my_tbl_name','id_col_name'));"
    */
+  @pg_query($vink, 'COMMIT');
 }
 
 require_once('header.inc.php');
@@ -19,7 +36,7 @@ $contacts_res = @pg_query_params($vink, 'SELECT * FROM vink_contacts WHERE user_
                                  array($vink_userid));
 ?>
 <h1>Create new wave</h1>
-<form class='new-wave' method='compose.php' action='post'>
+<form class='new-wave' action='compose.php' method='post'>
   <div style='float:left'>
     <textarea name='vink-message' cols='80' rows='25'></textarea><br />
     <input type='submit' value='Save' />
