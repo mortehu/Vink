1a0d7a0d1605b33ba7d6931ce09c663e15619174
Morten Hustveit <morten@rashbox.org>
2009-12-29 09:10:50 +0100

    Copied MIME code from mailr.

--- mail-internal.h
+++ mail-internal.h
@@ -10,3 +10,22 @@ struct tuples
 {
   ARRAY_MEMBERS(struct tuple);
 };
+
+enum content_type
+{
+    ct_text_plain,
+    ct_message_rfc822,
+    ct_multipart_mixed,
+    ct_multipart_alternative,
+    ct_multipart_related,
+    ct_multipart_signed,
+    ct_multipart_other,
+    ct_other
+};
+
+enum content_transfer_encoding
+{
+    cte_quoted_printable,
+    cte_base64,
+    cte_literal
+};
--- rfc2822.c
+++ rfc2822.c
@@ -3,10 +3,17 @@
 #endif
 
 #include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <stdio.h>
+
+#include <iconv.h>
+
+#include "arena.h"
+#include "array.h"
+#include "vink.h"
 
 #include "mail-internal.h"
 
@@ -239,4 +246,430 @@ VINK_rfc2822_parse(const char* buf, const char* buf_end,
     }
 }
 
-/* vim: set sts=2 sw=2 et :*/
+static void
+rtrim(char* string)
+{
+  char* c = string + strlen(string);
+
+  while (c > string && isspace(*(c - 1)))
+    --c;
+
+  *c = 0;
+}
+
+static int
+convert_to_utf8(const char* input, size_t input_size, char** ret_output, size_t* output_size, const char* charset)
+{
+  size_t output_alloc = input_size * 3 / 2; /* UTF-8 is larger than almost any other encoding */
+  size_t output_remaining = output_alloc;
+  char* output;
+  char* o;
+
+  iconv_t cd = iconv_open("utf-8", charset);
+
+  if (cd == (iconv_t) -1)
+    return -1;
+
+  output = malloc(output_alloc);
+  o = output;
+
+  while (input_size)
+  {
+    if((size_t) -1 == iconv(cd, (char**) &input, &input_size, &o, &output_remaining))
+    {
+      if(E2BIG == errno || output_remaining == 0)
+      {
+        size_t old_alloc, old_offset;
+
+        old_alloc = output_alloc;
+        old_offset = o - output;
+        output_alloc += input_size * 2;
+        output_remaining += output_alloc - old_alloc;
+        output = realloc(output, output_alloc);
+        o = output + old_offset;
+      }
+      else
+      {
+        *o++ = '_';
+        ++input;
+        --input_size;
+        --output_remaining;
+      }
+    }
+  }
+
+  *output_size = o - output;
+  *ret_output = output;
+
+  assert(*output_size <= output_alloc);
+
+  iconv_close(cd);
+
+  return 0;
+}
+
+void
+mime_parse_content_type(const char* cts, char* type, size_t type_size,
+                        char* subtype, size_t subtype_size,
+                        char* charset, size_t charset_size,
+                        char* filename, size_t filename_size,
+                        char* part_delimiter, size_t part_delimiter_size)
+{
+  const char* type_delim = strchr(cts, '/');
+  size_t type_len;
+
+  if (!type_delim)
+    type_delim = cts;
+
+  type_len = type_delim - cts;
+
+  if (type_len >= type_size)
+    type_len = type_size - 1;
+
+  strncpy(type, cts, type_len);
+  type[type_len] = 0;
+
+  size_t subtype_len = strspn(type_delim + 1, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-");
+
+  if (subtype_len >= subtype_size)
+    subtype_len = subtype_size - 1;
+
+  strncpy(subtype, type_delim + 1, subtype_len);
+  subtype[subtype_len] = 0;
+
+  char* arg = strchr(type_delim, ';');
+
+  while (arg && *arg)
+    {
+      char* arg_end;
+
+      ++arg;
+
+      while (isspace(*arg))
+        ++arg;
+
+      if (!*arg)
+        break;
+
+      arg_end = strchr(arg + 1, ';');
+
+      if (!arg_end)
+        arg_end = strchr(arg + 1, 0);
+
+      char* arg_delim = strchr(arg, '=');
+
+      if (!arg_delim)
+        break;
+
+      char* key = strndupa(arg, arg_delim - arg);
+      char* value = strndupa(arg_delim + 1, arg_end - arg_delim - 1);
+
+      rtrim(value);
+
+      if (value[0] == '"' && value[strlen(value) - 1] == '"')
+        {
+          value[strlen(value) - 1] = 0;
+          ++value;
+        }
+
+      if (part_delimiter && !strcasecmp(key, "boundary"))
+        {
+          strncpy(part_delimiter, value, part_delimiter_size);
+          part_delimiter[part_delimiter_size - 1] = 0;
+        }
+      else if (filename && !strcasecmp(key, "name"))
+        {
+          if (strchr(value, '/'))
+            strncpy(filename, strrchr(value, '/') + 1, filename_size);
+          else
+            strncpy(filename, value, filename_size);
+
+          filename[filename_size - 1] = 0;
+        }
+      else if (!strcasecmp(key, "charset"))
+        {
+          strncpy(charset, value, charset_size);
+          charset[charset_size - 1] = 0;
+        }
+
+      arg = arg_end;
+    }
+}
+
+void
+mime_parse_content_disposition(const char* arg, char* filename,
+                               size_t filename_size)
+{
+  while (arg && *arg)
+    {
+      char* arg_end;
+
+      ++arg;
+
+      while (isspace(*arg))
+        ++arg;
+
+      if (!*arg)
+        break;
+
+      arg_end = strchr(arg + 1, ';');
+
+      if (!arg_end)
+        arg_end = strchr(arg + 1, 0);
+
+      char* arg_delim = strchr(arg, '=');
+
+      if (!arg_delim)
+        break;
+
+      char* key = strndupa(arg, arg_delim - arg);
+      char* value = strndupa(arg_delim + 1, arg_end - arg_delim - 1);
+
+      rtrim(value);
+
+      if (value[0] == '"' && value[strlen(value) - 1] == '"')
+        {
+          value[strlen(value) - 1] = 0;
+          ++value;
+        }
+
+      if (filename && !strcasecmp(key, "filename"))
+        {
+          if (strchr(value, '/'))
+            strncpy(filename, strrchr(value, '/') + 1, filename_size);
+          else
+            strncpy(filename, value, filename_size);
+
+          filename[filename_size - 1] = 0;
+        }
+
+      arg = arg_end;
+    }
+}
+
+void
+mime_parse(struct vink_message* result, struct arena_info *arena,
+           const char* input, size_t input_size)
+{
+  char part_delimiter[256] = { 0 };
+  char filename[64] = { 0 };
+  char type[32] = "text";
+  char subtype[32] = "plain";
+  char charset[16] = { 0 };
+
+  struct tuples headers;
+  const char* end;
+  const char* body;
+  size_t i;
+  const struct tuple *content_type_header = 0;
+  const struct tuple *transfer_encoding_header = 0;
+  const struct tuple *content_disposition_header = 0;
+
+  end = input + input_size;
+
+  ARRAY_INIT(&headers);
+
+  VINK_rfc2822_parse(input, end, &headers, &body);
+
+  for(i = 0; i < ARRAY_COUNT(&headers); ++i)
+    {
+      const struct tuple *h;
+
+      h = &ARRAY_GET(&headers, i);
+
+      if (!strncasecmp(h->key, "content-type", h->key_size))
+        content_type_header = h;
+      else if (!strncasecmp(h->key, "content-transfer-encoding", h->key_size))
+        transfer_encoding_header = h;
+      else if (!strncasecmp(h->key, "content-disposition", h->key_size))
+        content_disposition_header = h;
+    }
+
+  enum content_type ct = ct_other;
+  enum content_transfer_encoding cte = cte_literal;
+
+  if(content_type_header)
+    {
+      char* cts = strndupa(content_type_header->value, content_type_header->value_size);
+
+      mime_parse_content_type(cts, type, sizeof(type), subtype, sizeof(subtype), charset, sizeof(charset), filename, sizeof(filename), part_delimiter, sizeof(part_delimiter));
+
+      if (!strcasecmp(type, "text"))
+        {
+          if (!strcasecmp(subtype, "plain"))
+            ct = ct_text_plain;
+        }
+      else if(!strcasecmp(type, "message"))
+        {
+          if(!strcasecmp(subtype, "rfc822"))
+            ct = ct_message_rfc822;
+        }
+      else if (!strcasecmp(type, "multipart"))
+        {
+          if (!strcasecmp(subtype, "mixed"))
+            ct = ct_multipart_mixed;
+          else if (!strcasecmp(subtype, "alternative"))
+            ct = ct_multipart_alternative;
+          else if (!strcasecmp(subtype, "related"))
+            ct = ct_multipart_related;
+          else if (!strcasecmp(subtype, "signed"))
+            ct = ct_multipart_signed;
+          else
+            ct = ct_multipart_other;
+        }
+    }
+  else
+    ct = ct_text_plain;
+
+  if(!charset[0])
+    strcpy(charset, "US-ASCII");
+
+  if(transfer_encoding_header)
+    {
+      char* ctes = strndupa(transfer_encoding_header->value, transfer_encoding_header->value_size);
+      rfc2822_unfold(ctes);
+
+      if (!strcasecmp(ctes, "quoted-printable"))
+        cte = cte_quoted_printable;
+      else if (!strcasecmp(ctes, "base64"))
+        cte = cte_base64;
+    }
+
+  if(content_disposition_header)
+    {
+      char* cd = strndupa(content_disposition_header->value, content_disposition_header->value_size);
+
+      mime_parse_content_disposition(cd, filename, sizeof(filename));
+    }
+
+  if (   ct == ct_multipart_mixed
+      || ct == ct_multipart_alternative
+      || ct == ct_multipart_related
+      || ct == ct_multipart_signed
+      || ct == ct_multipart_other)
+    {
+      struct vink_message *parts = 0;
+      const char *i, *part_begin = 0, *part_end = 0;
+      size_t part_delimiter_len;
+      size_t pass, part_idx;
+
+      if(!part_delimiter[0])
+        return;
+
+      part_delimiter_len = strlen(part_delimiter);
+
+      for(pass = 0; pass < 2; ++pass)
+        {
+          part_idx = 0;
+
+          for(i = body; i < end - part_delimiter_len - 2; ++i)
+            {
+              if ((i == body || *(i - 1) == '\n')
+                  && *i == '-' && *(i + 1) == '-'
+                  && !memcmp(i + 2, part_delimiter, part_delimiter_len))
+                {
+                  if (part_begin)
+                    {
+                      part_end = i;
+
+                      if (part_begin != part_end)
+                        {
+                          if(pass == 0)
+                            ++result->part_count;
+                          else
+                            mime_parse(&parts[part_idx], arena,
+                                       part_begin, part_end - part_begin);
+
+                          ++part_idx;
+                        }
+                    }
+
+                  part_begin = i + part_delimiter_len + 2;
+
+                  while (isspace(*part_begin) && *part_begin != '\n')
+                    ++part_begin;
+
+                  if (*part_begin == '\n')
+                    ++part_begin;
+                }
+            }
+
+          if(pass == 0)
+            parts = arena_calloc(arena, sizeof(*result->parts) * result->part_count);
+        }
+
+      result->parts = parts;
+    }
+  else if (ct == ct_text_plain)
+    {
+      const char* decoded_body = body;
+      size_t body_size = end - body;
+
+      if (cte == cte_quoted_printable)
+        {
+          char* tmp = malloc(body_size);
+          const char* i = body;
+          char* o = tmp;
+
+          while (i != end)
+            {
+              if (*i == '=' && i + 2 < end)
+                {
+                  if (isspace(i[1]))
+                    i += 2;
+                  else
+                    {
+                      if (i[1] >= '0' && i[1] <= '9')
+                        *o = (i[1] - '0') << 4;
+                      else
+                        *o = (i[1] - 'A' + 0xA) << 4;
+
+                      if (i[2] >= '0' && i[2] <= '9')
+                        *o |= (i[2] - '0');
+                      else
+                        *o |= (i[2] - 'A' + 0xA);
+
+                      ++o;
+                      i += 3;
+                    }
+                }
+              else
+                *o++ = *i++;
+            }
+
+          body_size = o - tmp;
+          decoded_body = tmp;
+        }
+      else if (cte == cte_base64)
+        {
+          char* tmp = malloc(body_size);
+          int result;
+
+          result = base64_decode(tmp, decoded_body, body_size);
+
+          if (result >= 0)
+            {
+              body_size = result;
+              decoded_body = tmp;
+            }
+        }
+
+      if (strcasecmp(charset, "utf-8") && strcasecmp(charset, "us-ascii"))
+        {
+          char* tmp;
+          size_t new_size;
+
+          if (-1 != convert_to_utf8(decoded_body, body_size, &tmp, &new_size, charset))
+            {
+              if (decoded_body != body)
+                free((char*) decoded_body);
+
+              decoded_body = tmp;
+              body_size = new_size;
+            }
+        }
+
+      if (decoded_body != body)
+        free((char*) decoded_body);
+    }
+}
