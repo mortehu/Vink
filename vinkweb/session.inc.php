2b2a73654c9973d743dcc64f3921914c705b36de
Morten Hustveit <morten@rashbox.org>
2010-01-23 10:53:24 +0100

    Updated queries to match new schema.

--- vinkweb/session.inc.php
+++ vinkweb/session.inc.php
@@ -11,9 +11,9 @@ if(!$vink)
 if(array_key_exists('vink-user', $_POST) && array_key_exists('vink-secret', $_POST))
 {
   if(array_key_exists('session', $_COOKIE))
-    pg_query_params($vink, 'DELETE FROM vink_sessions WHERE id = $1', array($_COOKIE['session']));
+    pg_query_params($vink, 'DELETE FROM sessions WHERE id = $1', array($_COOKIE['session']));
 
-  $user_res = pg_query_params($vink, 'SELECT * FROM vink_users WHERE name = $1', array($_POST['vink-user']));
+  $user_res = pg_query_params($vink, 'SELECT * FROM users WHERE username = $1', array($_POST['vink-user']));
 
   if(!pg_num_rows($user_res))
   {
@@ -24,23 +24,23 @@ if(array_key_exists('vink-user', $_POST) && array_key_exists('vink-secret', $_PO
 
   $user_row = pg_fetch_array($user_res);
 
-  if($user_row['secret'] != $_POST['vink-secret'])
+  if($user_row['password'] != $_POST['vink-secret'])
   {
     require('403.php');
 
     exit;
   }
 
-  $vink_userid = $user_row['id'];
+  $vink_userid = $user_row['seqid'];
   $vink_sessionid = sha1($vink_userid . uniqid() . time() . $vink_secret);
   setcookie('session', $vink_sessionid, time() + 3600 * 24 * 365, '/', $_SERVER['HTTP_HOST']);
   
-  pg_query_params('INSERT INTO vink_sessions (id, user_id) VALUES ($1, $2)',
+  pg_query_params('INSERT INTO sessions (id, userid) VALUES ($1, $2)',
                   array($vink_sessionid, $vink_userid));
 }
 else if(array_key_exists('session', $_COOKIE))
 {
-  $session_res = pg_query_params($vink, 'SELECT user_id FROM vink_sessions WHERE id = $1', array($_COOKIE['session']));
+  $session_res = pg_query_params($vink, 'SELECT userid FROM sessions WHERE id = $1', array($_COOKIE['session']));
 
   if(!pg_num_rows($session_res))
   {
@@ -51,8 +51,8 @@ else if(array_key_exists('session', $_COOKIE))
 
   $session_row = pg_fetch_array($session_res);
 
-  $vink_sessionid = $session_row['id'];
-  $vink_userid = $session_row['user_id'];
+  $vink_sessionid = $_COOKIE['session'];
+  $vink_userid = $session_row['userid'];
 }
 else
 {
